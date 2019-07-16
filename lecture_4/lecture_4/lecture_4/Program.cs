2b95ac39550072b3db69d2cf32ac02093abfdcf2
Zinina Darya <black_suigintou@yahoo.com>
2019-07-16 23:39:15 +0300

    Done

---
+++ lecture_4/lecture_4/lecture_4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lecture_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = DateTime.Now.TimeOfDay;
+            int hours = data.Hours;
+            if (hours>=9&hours<12)
+            {
+                Console.WriteLine("Good morning,guys");
+            }
+            else if (hours>=12&hours<15)
+            {
+                Console.WriteLine("Good day,guys");
+            }
+            else if (hours>=15&hours<22)
+            {
+                Console.WriteLine("Good evening, guys");
+            }
+            else
+            {
+                Console.WriteLine("Good night, guys");
+            }
+            Console.Read();
+        }
+    }
+}
