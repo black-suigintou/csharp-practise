120d86158ba6dd2e2d49826836d118511e68ade0
Zinina Darya <black_suigintou@yahoo.com>
2019-07-16 21:05:13 +0300

    Task_02 Done

--- lecture_8/lecture_8/Task_02/Program.cs
+++ lecture_8/lecture_8/Task_02/Program.cs
@@ -10,7 +10,27 @@ namespace Task_02
     {
         static void Main(string[] args)
         {
+            object[] arr = new object[3];
+            arr[0] = "Hello";
+            arr[1] = "a";
+            arr[2]=32;
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.WriteLine("Измененный массив:");
+            arr[0] = arr[0] + ", guys!";
+            int num1 = Convert.ToInt32(arr[2]);
+            int num2 = num1 + 10;
+            arr[2] = num2;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
 
+
+            Console.Read();
         }
     }
 }
