b9e4947a742fd79ce6b2e36a3ed316100b7e1686
Zinina Darya <black_suigintou@yahoo.com>
2019-07-16 20:15:34 +0300

    Task_03 done

---
+++ lecture_8/lecture_8/Task_03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[7];
+            Random rnd = new Random();
+            for (int i = 0; i < 6; i++)
+            {                
+                int num = rnd.Next(0, 100);
+                arr[i] = num;
+            }
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Array.Sort(arr);
+            int maxValue = arr[arr.Length - 1];
+            Console.Write("Максимальное значение - ");
+            Console.WriteLine(maxValue);
+            Console.Read();
+        }
+    }
+}
