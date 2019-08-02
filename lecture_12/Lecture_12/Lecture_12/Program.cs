e048b6f425678f59dc0f3cc421696ca21cd724bf
Zinina Darya <black_suigintou@yahoo.com>
2019-08-02 15:15:46 +0300

    Done

--- lecture_12/Lecture_12/Lecture_12/Program.cs
+++ lecture_12/Lecture_12/Lecture_12/Program.cs
@@ -6,40 +6,42 @@ using System.Threading.Tasks;
 
 namespace Lecture_12
 {
-    struct rectangle
+    public struct rectangle
     {
-        public int width;
-        public int length;
-        rectangle(int width, int length)
+        public int width, length;       
+        public rectangle(int width, int length)
         {
             this.length = length;
-            this.width = width;
-            width = length;
-        }
+            this.width = width;            
+        }       
     }
     class Program
-    {     
-
+    {
         static void Main(string[] args)
         {
             Random rnd = new Random();
-            rectangle[] square = new rectangle [100];
+            rectangle[] square = new rectangle[100];
+            int counterForEquals = 0;
             for (int i = 0; i < square.Length; i++)
             {
                 square[i].length = rnd.Next(10);
+                square[i].width = square[i].length;
             }
             for (int i = 1; i < square.Length; i++)
             {
-                square[0].Equals(square[i]);
-                if (square[0].Equals(square[i]) == true) ;                                                 
-                                
+                square[0].length.Equals(square[i].length);
+                if (square[0].length.Equals(square[i].length) == true) 
+                    for (int j = 0; j < 1;j++)
+                    {
+                        counterForEquals += 1;
+                    }                 
             }
+            Console.WriteLine("Полученный массив:");
             for (int i = 0; i < square.Length; i++)
             {
-                Console.WriteLine(square[i]);
-            }
-
-            
+                Console.WriteLine(square[i].width); // здесь поставила .width чтобы показать, что square[i].width было присвоено значение square[i].length 
+            }            
+            Console.WriteLine($"Количество дупликатов {counterForEquals}");            
             Console.Read();
 
         }
