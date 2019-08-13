4ab72c9b190c73071c396caf1b7b84bfaef78272
Zinina Darya <black_suigintou@yahoo.com>
2019-08-13 23:41:09 +0300

    not done

---
+++ lecture_13/Lecture 13/Lecture 13/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Triangle trian = new Triangle();
+            //trian.len = rnd.Next(1, 10);
+            trian.height = rnd.Next(1, 10);
+            int square1 = trian.height.Square(rnd.Next(1, 10));
+           
+
+        }
+    }
+    public interface IShape
+    {
+        void Square(int len);
+    }
+
+    class ShapeSquare : IShape()
+    {
+        public void Square()
+        {            
+            int squareOFShapeSquare = len * len;
+        }
+    }
+    class Triangle: Shape
+    {
+        public int squreOfTriangle;
+        public int height;
+        public override void Square(int len)
+        {
+            this.squreOfTriangle = len * height / 2;
+        }
+
+    }
+    class Circle:Shape
+    {
+        int radius;
+        public override void Square(int len)
+        {
+            int squareOfCircle = 3 * len * len;
+        }
+    }
+}
